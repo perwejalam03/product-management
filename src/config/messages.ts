1d8de63844eb9186dc97208b5db7933787182854
perwejalam03 <perwejalam03@gmail.com>
2024-12-20 16:59:34 +0530

    message config file added

---
+++ src/config/messages.ts
@@ -0,0 +1,11 @@
+export const messages = {
+  REGISTER_SUCCESS: "Registration successful. Please check your email for verification.",
+  EMAIL_ALREADY_EXISTS: "Email already registered, Please login or use a different email.",
+  VERIFY_EMAIL_SUCCESS: "Email verified successfully",
+  EMAIL_UNVERIFIED: 'Please verify your email before logging in',
+  VERIFY_EMAIL_FAILED: "Invalid or expired verification code",
+  INVALID_CREDENTIALS: "Invalid credentials",
+  USER_NOT_FOUND: "User not found",
+  TOKEN_REQUIRED: 'Authentication token required',
+  TOKEN_REVOKED_OR_EXPIRED: "Token revoked or expired",
+};
