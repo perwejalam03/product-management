0615843fea08691c2f2e7d7ce0a31d99b989c871
perwejalam03 <perwejalam03@gmail.com>
2024-12-21 12:16:14 +0530

    product type described

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,26 +1,28 @@
 export interface Product {
-  id: number
-  name: string
-  description: string
-  price: number
-  stock: number
-  created_at: Date
-  updated_at: Date
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  image_filename: string | null;
+  created_at: Date;
+  updated_at: Date;
+  categories?: number[];
 }
 
 export interface CreateProductDTO {
-  name: string
-  description: string
-  price: number
-  stock: number
-  categories?: number[]
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  categories: number[];
 }
 
 export interface UpdateProductDTO {
-  name?: string
-  description?: string
-  price?: number
-  stock?: number
-  categories?: number[]
+  name?: string;
+  description?: string;
+  price?: number;
+  stock?: number;
+  categories?: number[];
 }
 
